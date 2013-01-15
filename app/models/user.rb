994d6881b15d2cebecc38794156577d91d50c860
Matt Critchlow <mcritchlow@ucsd.edu>
2013-01-14 17:22:41 -0800

    add password to user model

---
+++ app/models/user.rb
@@ -0,0 +1,30 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  name            :string(255)
+#  email           :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  password_digest :string(255)
+#
+
+class User < ActiveRecord::Base
+  attr_accessible :email, :name, :password, :password_confirmation
+  #Rails 3.x function that assumes a password and password confirmation. Required password_digest field
+  has_secure_password
+  before_save { |user| user.email = email.downcase }
+  
+  #validation
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  #name must be present, max length = 50 (not nil)
+  validates :name, presence: true, length: { maximum: 50 } 
+  #email must be present (not nil)
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+    uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+  
+  
+end
