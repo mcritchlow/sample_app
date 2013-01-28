7fd7ef98885823bc43dd086ff713a4c5227b2f37
Matt Critchlow <mcritchlow@ucsd.edu>
2013-01-27 20:39:15 -0800

    Finish sign in

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,1 +1,16 @@
 include ApplicationHelper
+
+#remove need for redundant tests for error message, use "have_error"
+#￼replace this: should have_selector('div.alert.alert-error', text: 'Invalid
+# with this: should have_error_message('Invalid')
+RSpec::Matchers.define :have_error_message do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-error', text: message)
+  end
+end
+
+def valid_signin(user)
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+end
